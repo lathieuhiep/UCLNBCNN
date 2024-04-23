fba7cc8dcfd34009c9233462e3b6de395b23fd73
lathieuhiep <khacdiepkma90@gmail.com>
2024-04-23 14:46:07 +0700

    update code

---
+++ src/com/techja/uclnbcnn/UCLNBCNN.java
@@ -0,0 +1,14 @@
+package com.techja.uclnbcnn;
+
+public class UCLNBCNN {
+    private int a, b;
+
+    public UCLNBCNN(int a, int b) {
+        this.a = a;
+        this.b = b;
+    }
+
+    public void timUCLN() {
+
+    }
+}
